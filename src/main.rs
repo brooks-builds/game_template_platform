67dac8b6ff38139976f98cebb229ff59a6be7881
Brooks Patton <brooks@brookzerker.com>
2020-12-14 07:26:46 -0700

    can have full screen and using correct backend

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use game_template_platform::GameState;
+use ggez::conf::{Backend, WindowMode};
 use ggez::{event, ContextBuilder, GameResult};
 
 fn main() -> GameResult {
-    let (context, event_loop) =
-        &mut match ContextBuilder::new("Platform Template", "Brooks Patton").build() {
-            Ok((context, event_loop)) => (context, event_loop),
-            Err(error) => panic!(error),
-        };
+    let window_mode = WindowMode::default()
+        .dimensions(1280.0, 720.0)
+        .maximized(true)
+        .resizable(true);
+    let backend = Backend::default().version(3, 1).gles();
+    let (context, event_loop) = &mut match ContextBuilder::new("Platform Template", "Brooks Patton")
+        .window_mode(window_mode)
+        .backend(backend)
+        .build()
+    {
+        Ok((context, event_loop)) => (context, event_loop),
+        Err(error) => panic!(error),
+    };
 
     let game_state = &mut GameState::new(context)?;
 
